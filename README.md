8c6e689843637f7d90fa8cf39714962ceb44d087
Rukai Yu <rukai.yu@ringcentral.com>
2024-08-20 17:04:50 +0800

    update code

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # teams-simple-download
+
+
+## How to work?
+ - run `yarn`
+ - run `yarn start`
+ - run `ngork http 9083`, then got the https link of the URL, copy that
+ - modify the app/manifest.json, paste the URL of the static tab content URL, save file (note you may also need to change the "version" of the manifest)
+ - zip all files under the `app` folder to a `zip` file, upload to MS Teams app
+  
+
+## How to check issues
+  - In the browser app, click the Download file button, it works to get a txt file
+  - In new Teams desktop client, click the Download button, it is not able to download txt file
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 const port = 9083;
@@ -8,19 +9,31 @@ const port = 9083;
 // Middleware to parse cookies
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "src", "index.html");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading HTML file");
+      return;
+    }
+    res.send(data);
+  });
+});
+
 // Endpoint to download a text file
-app.get('/download', (req, res) => {
+app.get("/download", (req, res) => {
   // Check if the cookie `isAdmin` is set to true
-  if (req.cookies.isAdmin === 'true') {
+  if (req.cookies.isAdmin === "true") {
     res.setHeader(
-      'Content-disposition',
-      'attachment; filename=random-file.txt',
+      "Content-disposition",
+      "attachment; filename=random-file.txt"
     );
-    res.setHeader('Content-type', 'text/plain;charset=utf-8');
-    res.send('hello');
+    res.setHeader("Content-type", "text/plain;charset=utf-8");
+    res.send("hello");
   } else {
     // If cookie validation fails, respond with a 401 error
-    res.status(401).json({ error: 'invalid request' });
+    res.status(401).json({ error: "invalid request" });
   }
 });
 
